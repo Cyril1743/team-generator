3901c5809f8ba93e92a526800df2415c14eb91c9
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-02-22 17:01:34 -0600

    Added functions to write data to file

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ function final(question) {
             writeManagers(managers)
             writeEngineers(engineers)
             writeInterns(interns)
-            fs.appendFile('index.html', end, (err) => err ? console.log(err) : console.log("Done!"))
         }
     })
 }
@@ -85,4 +84,59 @@ function iQuestion(data1, question) {
         interns.push(new Intern(data1.TMname, data1.TMId, data1.TMemail, data.school))
         final(finalQuestion)
     })
+}
+//functions to write the data to the index.html file
+function writeData() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <title>My team</title>
+    </head>
+    <body>
+        <header class="position-absolute top-0 start-50 bg-primary d-block p-2">My team</header>`
+}
+function writeManagers(array) {
+    array.forEach(element => {
+        var content = `<div class = 'card' style="width: 18rem">
+                    <div class ='card-body'>
+                        <h5 class = 'card-title'>${element.getName()}, ${element.getRole()}</h5>
+                        <p class = 'card-text'> Id: ${element.getId()}</p>
+                        <p class = 'card-text'> <a href="mailto:${element.getEmail()}">${element.getEmail()}</a></p>
+                        <p class = 'card-text'>Office:${element.officeNumber}</p>
+                    </div>
+                </div>`
+        fs.appendFile("index.html", content, (err) => err ? console.log(err) : console.log("Successfully added Managers"))
+    });
+
+}
+function writeEngineers(array) {
+    array.forEach(element => {
+        var content = `<div class = 'card' style="width: 18rem">
+                    <div class ='card-body'>
+                        <h5 class = 'card-title'>${element.getName()}, ${element.getRole()}</h5>
+                        <p class = 'card-text'> Id: ${element.getId()}</p>
+                        <p class = 'card-text'> <a href="mailto:${element.getEmail()}">${element.getEmail()}</a></p>
+                        <p class = 'card-text'>GitHub:${element.getGithub()}</p>
+                    </div>
+                </div>`
+        fs.appendFile("index.html", content, (err) => err ? console.log(err) : console.log("Successfully added Engineers"))
+    });
+}
+function writeInterns(array) {
+    array.forEach(element => {
+        var content = `<div class = 'card' style="width: 18rem">
+                    <div class ='card-body'>
+                        <h5 class = 'card-title'>${element.getName()}, ${element.getRole()}</h5>
+                        <p class = 'card-text'> Id: ${element.getId()}</p>
+                        <p class = 'card-text'> <a href="mailto:${element.getEmail()}">${element.getEmail()}</a></p>
+                        <p class = 'card-text'>School:${element.getSchool()}</p>
+                    </div>
+                </div>`
+        fs.appendFile("index.html", content, (err) => err ? console.log(err) : console.log("Successfully added Interns"))
+    });
+    fs.appendFile('index.html', end, (err) => err ? console.log(err) : console.log("Done!"))
 }
